1613f06c5ebfbe69c1aafacdcfc7d0a44754786e
Igor Souza <igormcsouza@gmail.com>
2019-07-12 13:56:02 -0300

    start building models

--- application/rafinhas/models.py
+++ application/rafinhas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField()
+    senha = models.CharField()
+
+    # Usar JWT...
