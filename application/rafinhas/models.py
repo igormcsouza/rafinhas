b59fa8eb15e8353f9c72fea52b56b4f62c156475
Igor Souza <igormcsouza@gmail.com>
2019-07-12 21:09:14 -0300

    added jwt authentication

--- application/rafinhas/models.py
+++ application/rafinhas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField()
-    senha = models.CharField()
+    nome = models.CharField(max_length=10)
+    senha = models.CharField(max_length=20)
 
     # Usar JWT...
