eb5c2ac6b9a0d8d88d745bf691b3304965490785
Igor Souza <igormcsouza@gmail.com>
2019-07-12 21:16:08 -0300

    open space to minor changes at django

--- application/application/urls.py
+++ application/application/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+# Authetication token generator!
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
