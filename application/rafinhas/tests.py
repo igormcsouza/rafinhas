addc6a6caa3a63d440105cfea74c4a3d223f71a0
Igor Souza <igormcsouza@gmail.com>
2019-07-16 14:10:21 -0300

    implementing testes

--- application/rafinhas/tests.py
+++ application/rafinhas/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . import models
 
-# Create your tests here.
+class Fluxo(TestCase):
+    def novoCliente():
+        cliente = models.Cliente(
+            nome="Sergio Mouro", 
+            telefone1="85 987451369", 
+            telefone2="85 999763258"
+        )
+        cliente.save()
+        carro = models.Carro(placa="DEW8733", modelo="CHEVROLET", cliente=cliente)
+        carro.save()
